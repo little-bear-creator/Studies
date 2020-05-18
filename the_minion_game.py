8ad2618d40353d1da73921665773679835cc389b
little-bear-creator <matias.vtk@gmail.com>
2020-05-18 16:12:27 +0200

    First draft for minion game, not working

--- the_minion_game.py
+++ the_minion_game.py
@@ -1,5 +1,6 @@
 import string
-
+# TO DO : find another algorithm to count occurences of word in bigword
+# TO DO : find another algorithm for this game... Look at the exemple on HackerRank
 # Return "True" if char is a vowel
 def isVowel(char):
     vowels = "AEIOU"
@@ -11,13 +12,23 @@ def isVowel(char):
 
 # Count how many occurences of "word" in "bigword" exist, return an int
 # When calling the function write 0 for argument 'n'
+# Specification : argument word has to be built with string extracted from bigword
 def countOccurences(word, bigword, n):
 
     # x is a tuple : (char before word, word, char after word)
     x = bigword.partition(word)
+    # If word == bigword, we still have one occurence
+    if(word == bigword):
+        n += 1
     # if I can continue to test occurences
-    if(x[2] != ""):
+    elif(x[2] != ""):
         n = countOccurences(word, x[2], (n+1))
+    # In this case x[2] == "" so I can't conitnue to count occurences
+    # But word can still be a part of bigword, if word represent the last part of BANANA 
+    # For exemple : NANA with word BANANA
+    # wrong, count everything in double
+    #elif(x[0] != ""):
+        #n += 1
     return n
 
 def minion_game(string):
@@ -42,36 +53,52 @@ def minion_game(string):
         # for each letter of word "string"
         for index in range(len(string)):
             # Save the rest of string starting at index
-            bigword = string[index::]
+            #bigword = string[index::]
 
             # Test if char is a Vowel, then Kevin can play
             if(isVowel(string[index]) == "True"):
 
-                for i in range(len(bigword)):
-                    # iterate through bigword by taking a word more and more bigger
-                    # if i != 0 we have already search and count string[index] in string
+                for i in range(len(string)):
+                    # iterate through string by taking a word more and more bigger
+                    # if i != 0 we have already searched and counted string[index] in string
                     if(i != 0):
-                        word = string[index::index+i]
+                        word = string[index:index+i]
                     # Case where i == 0 and we just have to search string[index]
                     else:
                         word = string[index]
                     # check if word hasn't been used before
                     if(result_list_kevin.count(word) == 0):
-                        result_kevin += countOccurences(word, bigword, 0)
+                        result_kevin += countOccurences(word, string, 0)
                         result_list_kevin.append(word)
             else:
                 # Stuart can play
-                for i in range(len(bigword)):
-                    # iterate through bigword by taking a word more and more bigger
-                    # if i != 0 we have already search and count string[index] in string
-                    if(i != 0):
-                        word = string[index::index+i]
-                        result_stuart += countOccurences(word, bigword, 0)
-                    # Case where i == 0 and we just have to search string[index]
-                    else:
-                        word = string[index]
-                        result_stuart += countOccurences(word, bigword, 0)
 
+                for i in range(len(string)):
+                    
+                    # Case where i == index and we just have to search string[index]
+                    if(i == index):
+                        word = string[index]
+                    # We start searching new words at index
+                    elif(i > index):
+                        # TO DO : BANANA doesn't appear
+                        word = string[index:(index+i)]
+                    # In this case, i != 0 and i <= index, we don't search for new words
+                    else:
+                        word = ""
+                    print("Word before test is : "+word+", value of i and index are :"+str(index)+" "+str(i))
+
+                    # check if word hasn't been used before
+                    if((word != "") and (result_list_stuart.count(word) == 0)):
+                        print("Word found is "+word+" for Stuart")
+                        result_stuart += countOccurences(word, string, 0)
+                        print("result for Stuart is "+str(result_stuart))
+                        result_list_stuart.append(word)
+                        print("")
+
+        #Print results to debug calcul
+        print("----- "+"Stuart a "+str(result_stuart))
+        
+        print("----- "+"Kevin a "+str(result_kevin))
         
         # Test who wins
         if(result_kevin > result_stuart):
@@ -80,12 +107,15 @@ def minion_game(string):
             print("Stuart "+str(result_stuart))
         else:
             print("Draw")
+
+        #End of result
+        print("------- END of calcul -----------")
     
 
 if __name__ == '__main__':
-    s = "BAANANAS"
-    minion_game(s)
-    s = "BANANANAAAS"
-    minion_game(s)
+    #s = "BAANANAS"
+    #minion_game(s)
+    #s = "BANANANAAAS"
+    #minion_game(s)
     s = "BANANA"
     minion_game(s)
